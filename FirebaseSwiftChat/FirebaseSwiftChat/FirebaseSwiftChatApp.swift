66b5ed402c65a0a5fbf82d8371c25e5b6b5640db
Linh V Fowler <htlinhvu@gmail.com>
2025-01-28 21:00:43 -0500

    initial commit

---
+++ FirebaseSwiftChat/FirebaseSwiftChat/FirebaseSwiftChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSwiftChatApp.swift
+//  FirebaseSwiftChat
+//
+//  Created by Linh Vu on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSwiftChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirebaseSwiftChat/FirebaseSwiftChatTests/FirebaseSwiftChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSwiftChatTests.swift
+//  FirebaseSwiftChatTests
+//
+//  Created by Linh Vu on 1/28/25.
+//
+
+import Testing
+@testable import FirebaseSwiftChat
+
+struct FirebaseSwiftChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
